c589b520ac21111cd5a65578634fa7900ae8fde6
surendraguduri <88442867+surendraguduri@users.noreply.github.com>
2021-08-16 18:19:17 +0530

    Update string concatenate.cpp

--- string concatenate.cpp
+++ string concatenate.cpp
@@ -2,12 +2,30 @@
 #include<string.h>
 int main()
 {
-	int c;
-	char s[100],s1[100];
-	scanf("%s ",s);
-	scanf("\n%s",s1);
-	strcat(s1,s);
-	printf("\n%s",s1);
-	c=strlen(s1);
-	printf("\n%d ",c);
+    char s[100];
+    char s1[100];
+    char d[100];
+    int i=0,j=0;
+    scanf("%s",s);
+    scanf("%s",s1);
+    //strcat(s1,s);
+    while(s1[i]!='\0')//traversing the string untill null character encounter 
+    {
+        d[i]=s1[i];
+        i++;
+        
+    }
+    
+    while(s[j]!='\0')//copying source string into destination string till null character encounter
+    {
+        d[i]=s[j];
+        i++;
+        j++;
+    }
+    d[i]='\0';//adding null character to destination string
+    printf("%s\n",d);
+
+    printf("%ld\n",sizeof(d));//return total capacity of the string %ld is given it returns long unsigned integers
+    printf("%ld",strlen(d));//total length of the string without null character %ld is given it returns long unsigned integers
+    return 0;
 }
